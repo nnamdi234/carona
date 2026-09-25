830b5c373b33d65ee9e66eae6a3584e4195768c4
Nnamdi <nnamdiau@gmail.com>
2026-09-25 13:11:01 +0100

    feat: add lifespan to FastAPI initialization for improved resource management

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ app = FastAPI(
     title="Carona API", 
     version= "0.1.0",
     description="API for Carona carpooling service",
-    summary="This API allows users to create and manage carpooling rides, view available rides, and join existing rides."
+    summary="This API allows users to create and manage carpooling rides, view available rides, and join existing rides.",
+    lifespan=lifespan
 )
 
 app.include_router(user_router)
